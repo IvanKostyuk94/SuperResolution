4599b4b91946e863ab71ec55920756d8e07fbce6
IvanKostyuk94 <kostyuk@hotmail.de>
2022-11-07 17:42:39 +0100

    Class who's objects are trainable models

--- src/building_blocks/network_model.py
+++ src/building_blocks/network_model.py
@@ -1,27 +1,42 @@
-import tensorflow as tf
 from tensorflow.keras import layers
+import tensorflow as tf
 
 
 class NetworkModel:
+    """
+    Creates a trainable network model
+    """
+
     def __init__(
-        self,
-        network,
-        optimizer,
-        in_x=None,
-        in_y=None,
-        in_z=None,
-        channels=1,
-        dim3=False,
+        self, network_function, optimizer, input_shape=(None, None, None, 1)
     ):
-        if in_z is None:
-            self.input = layers.Input(shape=(in_x, in_y, channels))
-        else:
-            self.input = layers.Input(shape=(in_x, in_y, in_z, channels))
+        """
+        Initialize the network model
 
-        self.model = tf.keras.Model(
-            inputs=self.input, outputs=network(self.input)
-        )
+        Args:
+            network_function (function): The function of the network
+            optimizer (tensorflow.python.keras.optimizer_v2): optimizer to be used for this network
+            input_shape (tuple, optional): The shape of the network input, default is for a 3D generator.
+                                        Critic requires the actual shape of the generator output.
+                                        Defaults to (None, None, None, 1).
+        """
+        self.network_function = network_function
         self.optimizer = optimizer
+        self.input_shape = input_shape
+        self.network_model = self._build_model()
 
-    def apply(self, input_tile):
-        return self.model.predict(input_tile)
+    def _build_model(self):
+        """
+        Builds a keras Model out of the network funciton
+
+        Returns:
+            tensorflow.python.keras.engine.training.Model: Trainable keras model
+        """
+        input = layers.Input(shape=self.input_shape)
+        model = tf.keras.Model(
+            inputs=input, outputs=self.network_function(input)
+        )
+        return model
+
+    def apply(self, data):
+        return self.network_model(data)
