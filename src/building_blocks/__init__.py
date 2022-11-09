a6a67b6d3ce86e46153d61f91f40d3877860ef2b
IvanKostyuk94 <kostyuk@hotmail.de>
2022-11-09 16:34:35 +0100

    Created __init__.py files for all subdirs

---
+++ src/building_blocks/__init__.py
@@ -0,0 +1,5 @@
+from full_model import FullModel
+from run_tracker import RunTracker
+from network_model import NetworkModel
+from dataset_gen import DatasetGenerator
+from loss import Loss
---
+++ src/loss/__init__.py
@@ -0,0 +1,7 @@
+from l2_loss import l2_loss
+from loss_gan_only import (
+    first_critic_loss,
+    first_generator_loss,
+    second_critic_loss,
+    second_generator_loss,
+)
